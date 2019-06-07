0a601dfe20e58dcb5f697cfdee8caf796a16e819
sth23 <47009471+sth23@users.noreply.github.com>
2019-06-07 10:32:49 -0400

    Updated from Brython Server: 6/7/2019 10:32:51 AM

--- lunar-lander-game.py
+++ lunar-lander-game.py
@@ -1,1 +1,47 @@
+"""
+Final Project: Lunar Lander Game
+Author: Sean
+Credit: Tutorials
+Assignment: Create an old-school lunar lander game
+"""
 
+from ggame import App, RectangleAsset, CircleAsset, LineAsset, ImageAsset, Frame, Sprite, LineStyle, Color
+import math
+import random
+
+# Colors & lines
+red = Color(0xff0000, 1.0)
+orange = Color(0xffa500, 1.0)
+yellow = Color(0xffff00, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+purple = Color(0x800080, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+gray = Color(0x888888, 0.5)
+noline = LineStyle(0, black)
+whiteline = LineStyle(1, white)
+
+class Explosion(Sprite):
+    asset = ImageAsset("explosion1.png", Frame(0,0,128,128), 10, 'horizontal')
+    
+    def __init__(self, position):
+        super().__init__(Explosion.asset, position)
+        self.fxcenter = self.fycenter = 0.5
+        self.countup = 0
+        self.countdown = 10
+        
+    def step(self):
+        # Manage explosion animation
+        if self.countup < 10:
+            self.setImage(self.countup%10)
+            self.countup += 1
+        else:
+            self.setImage(self.countdown%10)
+            self.countdown -= 1
+            if self.countdown == 0:
+                self.destroy()
+
+class Turrain(Sprite):
+    def __init__(self, asset, position):
+        super().__init__(asset, position)

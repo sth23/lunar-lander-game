339925bd74271e606fabe4aefa8cb648b16438e0
sth23 <47009471+sth23@users.noreply.github.com>
2019-06-07 21:55:33 -0400

    Updated from Brython Server: 6/7/2019 9:55:35 PM

--- lunar-lander-game.py
+++ lunar-lander-game.py
@@ -62,6 +62,7 @@ class Lander(Sprite):
         self.paused = True
         self.fxcenter = self.fycenter = 0.5
         self.speed = 0
+        self.landed = False
         
         LunarLanderGame.listenKeyEvent("keydown", "up arrow", self.thrustOn)
         LunarLanderGame.listenKeyEvent("keydown", "right arrow", self.rotateRight)
@@ -84,7 +85,7 @@ class Lander(Sprite):
         self.paused = not self.paused
         
     def step(self):
-        if self.paused == False:
+        if self.paused == False & self.landed == False:
             self.x += self.vx
             self.y += self.vy
             self.vy += self.gravity
@@ -122,7 +123,7 @@ class LunarLanderGame(App):
                     Explosion((lander.x, lander.y))
                     lander.destroy()
                 else:
-                    lander.paused = not lander.paused
+                    lander.landed = True
                 
         for explosion in self.getSpritesbyClass(Explosion):
             explosion.step()

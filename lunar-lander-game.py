4d605ceafbf6c6c34ad56ddbc55d282c40b412b4
sth23 <47009471+sth23@users.noreply.github.com>
2019-06-07 21:49:50 -0400

    Updated from Brython Server: 6/7/2019 9:49:52 PM

--- lunar-lander-game.py
+++ lunar-lander-game.py
@@ -118,7 +118,7 @@ class LunarLanderGame(App):
         for lander in self.getSpritesbyClass(Lander):
             lander.step()
             if lander.collidingWithSprites(Turrain):
-                if lander.rotation > 1 or lander.rotation < 1:
+                if lander.rotation > 1 or lander.rotation < -1:
                     Explosion((lander.x, lander.y))
                     lander.destroy()
                 

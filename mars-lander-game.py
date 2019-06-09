2b5255a7276227c46b72fbf41600b9e73626a529
sth23 <47009471+sth23@users.noreply.github.com>
2019-06-09 11:34:19 -0400

    Updated from Brython Server: 6/9/2019 11:34:19 AM

--- mars-lander-game.py
+++ mars-lander-game.py
@@ -50,10 +50,16 @@ class LandingArea(Sprite):
     def __init__(self, asset, position):
         super().__init__(asset, position)
         
-class Fuel(Sprite):
+class FuelBar(Sprite):
     def __init__(self, fuel):
         self.fuelbar = RectangleAsset(fuel, 10, noline, red)
         super().__init__(self.fuelbar, (10, 10))
+        FuelLevel(fuel)
+        
+class FuelLevel(Sprite):
+    def __init__(self, fuel):
+        self.fuellevel = TextAsset("Fuel: " + str(fuel))
+        super()__init__(self.fuellevel, (10, 10))
         
 class Lander(Sprite):
     ship = PolygonAsset([(0,15), (7.5,0), (15,15), (7.5,7.5)], noline, black)
